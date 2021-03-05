7d3903bb28119d8d077d0b59ccd223db36359d8f
jack battle <jackonno78@hotmail.com>
2021-03-04 21:45:04 -0600

    added table and table rows component

---
+++ src/components/Table/Table.js
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from 'react';
+import { useGet } from "../../hooks/API"
+import "./Table.css"
+import Button from "../Button/Button"
+import { EmployeeContext } from '../EmployeeContext/EmployeeContext';
+
+function Table() {
+    const [url] = useState("https://randomuser.me/api/?results=10")
+    const { sortFunc } = useGet(url);
+    const { displayedEmployees} = useContext(EmployeeContext)
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <td onClick={() => sortFunc("name")}><Button>First Name</Button></td>
+                    <td>Last Name</td>
+                    <td>UID</td>
+                    <td>Gender</td>
+                    <td>E-mail</td>
+                    <td onClick={() => sortFunc("age")}><Button>Age</Button></td>
+                </tr>
+            </thead>
+            <tbody>
+                {displayedEmployees.map(employee => {
+                    return (
+                        <tr key={employee.login.uuid}>
+                            <td>{employee.name.first}</td>
+                            <td>{employee.name.last}</td>
+                            <td>{employee.login.uuid}</td>
+                            <td>{employee.gender}</td>
+                            <td>{employee.email}</td>
+                            <td>{employee.dob.age}</td>
+                        </tr>
+                    )
+                }
+                )}
+            </tbody>
+        </table>
+    )
+}
+export default Table;
---
+++ src/components/TableRow/TableRow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./TableRow.css"
+
+function TableRow(){
+    return(
+        <tr>
+            <td>Jack</td>
+            <td>Jackson</td>
+            <td>91025390</td>
+            <td>Male</td>
+            <td>jack@jackson.com</td>
+            <td>24</td>
+        </tr>
+    )
+}
+export default TableRow;
